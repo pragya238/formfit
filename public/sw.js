f3a964d6198ccd8d6508e64f1f29412654f02980
pragya238 <kashyappragya23@gmail.com>
2026-09-11 00:31:38 +0530

    Harden file access and surface extension workflow

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "formfit-shell-v1";
+const CACHE_NAME = "formfit-shell-v2";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add("/")));
